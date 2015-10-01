40bdfcf5c2e772dd0e48a9baa315f99cededb02d
Yuzo <hyzgog@gmail.com>
2015-10-01 17:59:28 +0800

    complete preview window

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -33,6 +33,7 @@ namespace ModelQuestion {
                     MessageBox.Show("No usable test case, the directory choosed is incorrect", "Incorrect directory", MessageBoxButton.OK, MessageBoxImage.Stop);
                 } else {
                     OptionGrid.IsEnabled = true;
+                    CaseCount.Maximum = CaseCount.Value = Questions.Length;
                 }
             }
         }
@@ -40,6 +41,18 @@ namespace ModelQuestion {
         private void StartButton_Click(object sender, RoutedEventArgs e) {
             Console.Write(sender);
             Console.WriteLine(e);
+        }
+
+        private void SerialRadio_Checked(object sender, RoutedEventArgs e) {
+            if (CaseCount != null) {
+                CaseCount.IsEnabled = false;
+            }
+        }
+
+        private void RandomRadio_Checked(object sender, RoutedEventArgs e) {
+            if (CaseCount != null) {
+                CaseCount.IsEnabled = true;
+            }
         }
     }
 }
